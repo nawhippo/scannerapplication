e3bb042d1e1ffc236ecbe7a6fa10da30b61858c2
whipm <whipmiester@hotmail.com>
2024-07-22 13:33:37 -0400

    altered controller to account for new controller wrapper

--- backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/medication/MedicationController.java
+++ backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/medication/MedicationController.java
@@ -1,45 +1,51 @@
 package nategroup.medicationScanner.medication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MedicationController {
     @Autowired
     private final MedicationService medicationService;
-    MedicationController(MedicationService medicationService){
+
+    MedicationController(MedicationService medicationService) {
         this.medicationService = medicationService;
     }
+
     @GetMapping("/getMedication/{medicationId}")
-    public ResponseEntity<Medication> getMedication(@RequestParam Long medicationId){
+    public Medication getMedication(@RequestParam Long medicationId) {
         Medication medication = medicationService.getMedicine(medicationId);
-        if (medication == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(medication);
-    }
-    @GetMapping("/getAllMedications")
-    public ResponseEntity<List<Medication>> getMedication(){
-        Optional<List<Medication>> medicines = Optional.ofNullable(medicationService.getAllMedicine());
-        if (medicines.isPresent()){
-            List<Medication> medications= medicines.get();
-            return ResponseEntity.ok(medications);
-        }
-        return ResponseEntity.notFound().build();
+        return medication;
     }
 
-    @PostMapping("/scanBarcode")
-    public ResponseEntity<Medication> ScanBarCode(@RequestParam Long medicationId) {
-        Optional<Medication> medication = Optional.ofNullable(medicationService.HandleBarcodeScan(medicationId));
-        if (medication.isPresent()) {
-            return ResponseEntity.ok(medication.get());
+    @GetMapping("/getAllMedications")
+    public List<Medication> getMedication() {
+        Optional<List<Medication>> medicines = Optional.ofNullable(medicationService.getAllMedicine());
+        if (medicines.isPresent()) {
+            List<Medication> medications = medicines.get();
+            return medications;
+        } else {
+            return null;
         }
-        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/scanBarcode")
+    public Medication ScanBarCode(@RequestParam Long medicationId, @RequestParam Optional<Integer> quantity) {
+        Optional<Medication> medication = Optional.ofNullable(medicationService.HandleBarcodeScan(medicationId, quantity));
+        if (medication.isPresent()) {
+            return medicationService.HandleBarcodeScan(medicationId, quantity);
+        }
+        return null;
+    }
+
+    @PostMapping("/createMedication")
+    public Medication createNewMedication(@RequestParam Medication medication){
+        return medicationService.addMedicine(medication);
     }
 }
