c6d283286750b1366c699194c12f413e15bbf1ea
whipm <whipmiester@hotmail.com>
2024-07-24 12:08:40 -0400

    created user service class

---
+++ backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/User/UserService.java
@@ -0,0 +1,38 @@
+package nategroup.medicationScanner.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(Long id, User userdetails) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setUsername(userdetails.getUsername());
+            user.setEmail(userdetails.getEmail());
+            user.setPassword(userdetails.getPassword());
+            return userRepository.save(user);
+        } else {
+            throw new RuntimeException("Shirt not found with id " + id);
+        }
+    }
+}
