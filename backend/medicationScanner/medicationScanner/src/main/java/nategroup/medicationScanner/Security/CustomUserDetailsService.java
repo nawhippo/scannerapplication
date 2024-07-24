f91d757b7e4dd015c1847047489da0dbe9bd464c
whipm <whipmiester@hotmail.com>
2024-07-24 14:49:11 -0400

    Created a custom user details service

--- backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/Security/CustomUserDetailsService.java
+++ backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/Security/CustomUserDetailsService.java
@@ -1,23 +1,31 @@
 package nategroup.medicationScanner.Security;
 
+import nategroup.medicationScanner.User.User;
 import nategroup.medicationScanner.User.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService extends UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
+
+    @Autowired
     private final UserRepository userRepository;
 
+    @Autowired
     public CustomUserDetailsService(UserRepository userRepository) {
+
         this.userRepository = userRepository;
     }
 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        return new CustomUserDetails(user);
     }
 }
