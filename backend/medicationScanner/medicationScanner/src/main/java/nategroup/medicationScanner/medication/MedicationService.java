68cff590b4dc6d9ff9c1a42546842fba82108298
whipm <whipmiester@hotmail.com>
2024-07-24 14:51:23 -0400

    added transaction feature

--- backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/medication/MedicationService.java
+++ backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/medication/MedicationService.java
@@ -1,15 +1,26 @@
 package nategroup.medicationScanner.medication;
 
+import nategroup.medicationScanner.Transaction.Transaction;
+import nategroup.medicationScanner.Transaction.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
 public class MedicationService {
+    @Autowired
     private final MedicationRepository medicationRepository;
-    MedicationService(MedicationRepository medicationRepository){
+    @Autowired
+    private final TransactionRepository transactionRepository;
+
+
+
+    MedicationService(MedicationRepository medicationRepository, TransactionRepository transactionRepository){
         this.medicationRepository = medicationRepository;
+        this.transactionRepository = transactionRepository;
     }
 
 
@@ -24,6 +35,7 @@ public class MedicationService {
     }
 
     public Medication addMedicine(Medication medication) {
+
         return medicationRepository.save(medication);
     }
     public Medication removeMedicine(Long medicineId) {
@@ -41,11 +53,13 @@ public class MedicationService {
     }
 
 
-    public Medication handleMedicationUpdate(Long medicationId, Integer quantity) {
+    public Medication handleMedicationUpdate(Long medicationId, Integer quantity, String username) {
         Optional<Medication> optionalMedication = medicationRepository.findById(medicationId);
         if (optionalMedication.isPresent()) {
             Medication medication = optionalMedication.get();
             medication.setSupply(medication.getSupply() + quantity);
+            Transaction transaction = new Transaction(medication.getName(), username, LocalDateTime.now(), quantity);
+            transactionRepository.save(transaction);
             return medicationRepository.save(medication);
         }
         return null;
