14f4d516241554db666a869dc46d89f0ef936957
whipm <whipmiester@hotmail.com>
2024-07-24 12:02:15 -0400

    added more table information

--- frontend/src/scripts/medicinetable.js
+++ frontend/src/scripts/medicinetable.js
@@ -1,17 +1,33 @@
 import axios from 'axios';
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchandrendertable();
+});
+
+
 function fetchandrendertable(){
-axios.get("./medication")
+axios.get("/getAllMedications")
 .then(response => {
-	const table = document.getElementById("medicationTable")
+	const table = document.getElementById("medicationTable");
 	response.data.forEach(element => {
-		row = document.createElement("tr")
-		cell = document.createElement("td");
-		cell.textContent = element
-		row.appendChild(cell)
-		table.append(row)
+		row = document.createElement("tr");
+
+		cellId = document.createElement("td");
+		cellId.textContent = element.id;
+		row.append(cellId);
+
+		cellName = document.createElement("td");
+		cell.textContent = element.name
+		row.append(cellName);
+
+		cellQuantity = document.createElement("td");
+		cell.textContent = element.supply;
+		row.append(cellQuantity);
+
+		table.append(row);
 	});
 })
 	.catch(error => {
-	console.log("error rendering medication table");
+	console.log("error rendering medication table : ", error);
 	});
 }
