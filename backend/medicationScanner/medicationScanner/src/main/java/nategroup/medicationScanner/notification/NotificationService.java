b61fe892d18b8f98bb588ad9952fa38a362f6fe4
whipm <whipmiester@hotmail.com>
2024-07-24 14:53:30 -0400

    added NotificationService w/ basic crud implementation

---
+++ backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/notification/NotificationService.java
@@ -0,0 +1,42 @@
+package nategroup.medicationScanner.notification;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    @Autowired
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    public List<Notification> getAllNotifications() {
+        return notificationRepository.findAll();
+    }
+
+    public Optional<Notification> getNotificationById(Long id) {
+        return notificationRepository.findById(id);
+    }
+
+    public Notification createNotification(Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    public Notification updateNotification(Long id, Notification notification) {
+        if (notificationRepository.existsById(id)) {
+            notification.setId(id);
+            return notificationRepository.save(notification);
+        }
+        return null;
+    }
+
+    public void deleteNotification(Long id) {
+        notificationRepository.deleteById(id);
+    }
+}
