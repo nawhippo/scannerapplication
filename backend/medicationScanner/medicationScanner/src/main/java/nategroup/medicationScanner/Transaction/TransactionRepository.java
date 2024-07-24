9539e5ddbb754d78f68cc3cf970e6b20d853a59c
whipm <whipmiester@hotmail.com>
2024-07-24 14:54:54 -0400

    added TransactionRepository

---
+++ backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/Transaction/TransactionRepository.java
@@ -0,0 +1,12 @@
+package nategroup.medicationScanner.Transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByCreatedBy(String name);
+}
