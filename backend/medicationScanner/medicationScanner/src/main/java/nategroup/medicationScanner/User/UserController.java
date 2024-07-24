8ad0febec8ac87f2d9261e3540b3a6386fd65aef
whipm <whipmiester@hotmail.com>
2024-07-24 12:05:54 -0400

    added user controller

---
+++ backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/User/UserController.java
@@ -0,0 +1,12 @@
+package nategroup.medicationScanner.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/account")
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
