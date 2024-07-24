63bb9457a24d1388ebd20a292f94fec78b10b9aa
whipm <whipmiester@hotmail.com>
2024-07-24 12:03:52 -0400

    added user

---
+++ backend/medicationScanner/medicationScanner/src/main/java/nategroup/medicationScanner/User/User.java
@@ -0,0 +1,52 @@
+package nategroup.medicationScanner.User;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "storefrontUsers")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+}
